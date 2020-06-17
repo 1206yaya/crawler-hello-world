f65b0ff7dab6d0a05a2eccad5de23a1ad5556c7c
1206yaya <1206yaya@gmail.com>
2020-06-17 19:44:10 +0900

    change driver path

--- src/test/java/app/Config.java
+++ src/test/java/app/Config.java
@@ -7,7 +7,8 @@ public class Config {
 	public final static String PROJ_DIR = Paths.get(System.getProperty("user.dir")).toString();
 	private static final String RESOURCES_DIR = Paths.get(PROJ_DIR, "src", "main", "resources").toString();
 	private static final String CHROMEDRIVER_DIR = Paths.get(RESOURCES_DIR, "chromedriver").toString();
-	public static final String CHROMEDRIVER_FOR_WIN_PATH = Paths.get(CHROMEDRIVER_DIR, "chromedriver72.exe").toString();
-	public static final String CHROMEDRIVER_FOR_LINUX_PATH = Paths.get(CHROMEDRIVER_DIR, "chromedriver").toString();
+//	public static final String CHROMEDRIVER_FOR_WIN_PATH = Paths.get(CHROMEDRIVER_DIR, "chromedriver72.exe").toString();
+	public static final String CHROMEDRIVER_FOR_WIN_PATH = Paths.get("C:\\Users\\aya-win10\\engineering\\tools\\crawler-driver\\chromedriver\\chromedriver72.exe").toString();
+	public static final String CHROMEDRIVER_FOR_LINUX_PATH = Paths.get("/var/lib/jenkins/crawler-dri ver/chromedriver/chromedrivar").toString();
 	
 }
